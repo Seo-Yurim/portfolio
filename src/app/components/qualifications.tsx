2b0917a68a8d7f389be0b1b93966a13abee08287
Seo-Yurim <yurim2204@naver.com>
2025-07-10 17:17:43 +0900

    FEAT: 한눈에보기 변환 구현

--- src/app/components/qualifications.tsx
+++ src/app/components/qualifications.tsx
@@ -7,9 +7,16 @@ export interface QualificationCardProps {
 }
 
 export function Qualifications() {
-  const [expanded, setExpanded] = useState<"skill" | "education" | "certificate" | "internship">(
-    "skill",
-  );
+  const [expanded, setExpanded] = useState<
+    "All" | "skill" | "education" | "certificate" | "internship"
+  >("skill");
+  const [viewAll, setViewAll] = useState<boolean>(false);
+
+  const handleView = () => {
+    setViewAll((prev) => !prev);
+    setExpanded("All");
+    if (expanded === "All") setExpanded("skill");
+  };
 
   return (
     <section id="qualifications" className="container flex h-screen flex-col gap-8 py-16">
@@ -17,11 +24,14 @@ export function Qualifications() {
         <h2 className="rounded-3xl bg-white px-6 py-2 text-3xl text-black shadow-right-down">
           Qualifications
         </h2>
-        <button className="rounded-3xl bg-blue-200 px-6 py-2 text-xl text-white shadow-right-down">
+        <button
+          onClick={handleView}
+          className="rounded-3xl bg-blue-200 px-6 py-2 text-xl text-white shadow-right-down"
+        >
           한 눈에 보기
         </button>
       </div>
-      <div className="flex w-full gap-2">
+      <div className={`${viewAll ? "grid grid-cols-2 grid-rows-2 gap-1" : "flex gap-2"} w-full`}>
         <SkillCard expanded={expanded === "skill"} onClick={() => setExpanded("skill")} />
         <EducationCard
           expanded={expanded === "education"}
