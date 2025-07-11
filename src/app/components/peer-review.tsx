554fcf83b157142bf4a67ad6ce5c5aea68c421c0
Seo-Yurim <yurim2204@naver.com>
2025-07-11 22:58:22 +0900

    FEAT: 피어리뷰 섹션 레이아웃 구현

--- src/app/components/peer-review.tsx
+++ src/app/components/peer-review.tsx
@@ -1,7 +1,10 @@
 export function PeerReview() {
   return (
-    <section id="peer-review" className="h-screen">
-      <p className="font-GWT">Contact</p>
+    <section id="peer-review" className="container flex h-screen flex-col gap-8">
+      <h2 className="w-fit rounded-3xl bg-white px-6 py-2 font-GWT text-3xl text-black shadow-right-down">
+        Peer Reviews
+      </h2>
+      <div className="grid h-[516px] grid-cols-3 gap-2 rounded-2xl bg-primary p-4"></div>
     </section>
   );
 }
