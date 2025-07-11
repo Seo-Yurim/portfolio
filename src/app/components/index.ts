67704fc983ed5fed6dac65c908e0847086d6c534
Seo-Yurim <yurim2204@naver.com>
2025-07-11 23:13:02 +0900

    FIX: 대소문자 파일명 인식 오류로 인한 임시 파일명

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,5 +1,5 @@
 export { Hero } from "./hero";
-export { Profile } from "./profile";
+export { Profile } from "./temp";
 export { Qualifications } from "./qualifications";
 export { ProjectList } from "./project-list";
 export { PeerReview } from "./peer-review";
