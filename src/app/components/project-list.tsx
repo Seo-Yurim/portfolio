f43a3d68db10eac6e63a1850333dd4f5de319020
Seo-Yurim <yurim2204@naver.com>
2025-07-11 22:53:07 +0900

    FEAT: 프로젝트 리스트 섹션 레이아웃 구현

--- src/app/components/project-list.tsx
+++ src/app/components/project-list.tsx
@@ -1,7 +1,20 @@
+import Tab from "@/components/tab";
+import Title from "@/components/title";
+
 export function ProjectList() {
   return (
-    <section id="project-list" className="h-screen">
-      <p className="font-GWT">Projects</p>
+    <section id="project-list" className="flex h-screen flex-col gap-8 py-16">
+      <Title title="Projects" />
+      <div className="container flex flex-col gap-4">
+        <Tab />
+        <div className="flex h-[400px] gap-4 overflow-x-scroll rounded-2xl bg-primary p-8">
+          <div className="w-[400px] flex-shrink-0 rounded-2xl bg-text-background p-8"></div>
+          <div className="w-[400px] flex-shrink-0 rounded-2xl bg-text-background p-8"></div>
+          <div className="w-[400px] flex-shrink-0 rounded-2xl bg-text-background p-8"></div>
+          <div className="w-[400px] flex-shrink-0 rounded-2xl bg-text-background p-8"></div>
+          <div className="w-[400px] flex-shrink-0 rounded-2xl bg-text-background p-8"></div>
+        </div>
+      </div>
     </section>
   );
 }
